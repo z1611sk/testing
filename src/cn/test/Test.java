9ea580e5381bd8d659ea4fe4833ed6466ba81f2b
Administrator111 <Administrator111@WIN-QLHBAL7KU80>
2017-12-18 13:36:22 +0800

    新的测试asadasd

--- src/cn/test/Test.java
+++ src/cn/test/Test.java
@@ -7,11 +7,14 @@ public class Test {
         System.out.println("sad此时一下啊啊啊啊aasdsadsa你好啊");
     }
 
-    public static void main(Strng[] arrgs){
+    public static void main(String[] arrgs){
         System.out.println("sad此时一下啊啊啊啊aasdsadsa你好啊");
     }
 
-    public static void mainasdas(Strng[] arrgs){
+    public static void mainasdas(String[] arrgs){
         System.out.println("sad此时一下啊啊啊啊aasdsadsa你好啊11111111111111111");
+    }
+    public static void mainasdas1(String[] arrgs){
+        System.out.println("分之开发 ");
     }
 }
