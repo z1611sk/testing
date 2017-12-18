bebfff4860968ee1e78754551bed248904dbfeaf
Administrator111 <Administrator111@WIN-QLHBAL7KU80>
2017-12-18 13:14:54 +0800

    新的测试asadasd

--- src/cn/test/Test.java
+++ src/cn/test/Test.java
@@ -4,6 +4,10 @@ public class Test {
 
 
     public void te1(){
-        System.out.println("sad此时一下啊啊啊啊aasdsadsa");
+        System.out.println("sad此时一下啊啊啊啊aasdsadsa你好啊");
+    }
+
+    public static void main(Strng[] arrgs){
+        System.out.println("sad此时一下啊啊啊啊aasdsadsa你好啊");
     }
 }
